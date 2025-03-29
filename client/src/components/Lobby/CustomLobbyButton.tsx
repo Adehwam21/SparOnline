dcb0440b5dbbfd28dfd5f09dec4838ef9f830c86
Adehwam21 <drewaaron96@gmail.com>
2025-03-29 21:43:14 +0000

    feat: add lobby component, and fix routes

---
+++ client/src/components/Lobby/CustomLobbyButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { IconType } from "react-icons";
+
+interface CustomButtonProps {
+  label: string;
+  icon: IconType;
+  onClick: () => void;
+}
+
+const CustomLobbyButton: React.FC<CustomButtonProps> = ({ label, icon: Icon, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="flex flex-col items-center justify-center bg-green-800 text-white p-6 rounded-lg shadow-md transition-transform duration-300 transform hover:scale-105 hover:text-yellow-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+    >
+      <Icon size={48} className="mb-2" />
+      <span className="text-lg font-semibold">{label}</span>
+    </button>
+  );
+};
+
+export default CustomLobbyButton;
