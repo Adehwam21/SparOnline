929457cc9144d52b04e460acbb127222439c5873
Adehwam21 <drewaaron96@gmail.com>
2025-03-29 21:44:05 +0000

    feat: add login and logout functionality

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,19 @@
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/routes.jsx';
-import { Provider } from 'react-redux';
-import { store } from './redux/reduxStore';
-import './index.css';
-import './axiosConfig.js';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/routes";
+import { Provider } from "react-redux";
+import { store } from "./redux/reduxStore";
+import "./index.css";
+import "./config/axiosConfig.js";
+import { Toaster } from "react-hot-toast";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <StrictMode>
       <Provider store={store}>
+        <Toaster toastOptions={{ duration: 5000 }} />
         <RouterProvider router={router} />
       </Provider>
     </StrictMode>
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -7,3 +7,44 @@ export interface LoginSignUpProps{
 export interface ProtectedRouteProps{
     children: ReactNode
 }
+
+// Toast options
+export const successToastOptions = {
+    style: {
+      border: '4px solid #0d542b',
+      padding: '10px',
+      color: '#0d542b',
+      backgroundColor: '#ffffff',
+    },
+    iconTheme: {
+      primary: '#0d542b',
+      secondary: '#ffffff',
+    },
+};
+
+export const errorToastOptions = {
+    style: {
+      border: '4px solid #c42501',
+      padding: '10px',
+      color: '#c42501',
+      backgroundColor: '#ffffff',
+    },
+    iconTheme: {
+      primary: '#c42501',
+      secondary: '#ffffff',
+    },
+};
+
+export const invalidPaawordOptions = {
+    style: {
+        border: '4px solid #c42501',
+        padding: '10px',
+        color: '#c42501',
+        backgroundColor: '#ffffff',
+      },
+      iconTheme: {
+        primary: '#c42501',
+        secondary: '#ffffff',
+      },
+    duration: 6000
+}
