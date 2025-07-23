d8df04d5bd3e3e0e6a24e58b2692cc85c70e20ed
Adehwam21 <drewaaron96@gmail.com>
2025-07-23 21:03:08 +0000

    feat: deduct entry fee

--- server/src/validation/game.ts
+++ server/src/validation/game.ts
@@ -8,7 +8,7 @@ export const createGameInput = Joi.object<ICreateGameInput>({
     maxPoints: Joi.string().required(), // Maximum points to win the game
     variant: Joi.string().required(), // Type of game (e.g., "classic", "custom")
     creator: Joi.string(), // Store player data
-    entryFee: Joi.string().optional(),
+    entryFee: Joi.number().optional(),
     bettingEnabled: Joi.boolean().optional(),
     roomType: Joi.string(),
     gameState: Joi.object(), // Store snapshot of game state
